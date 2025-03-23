499ed715da0d44ffea3b894743a5a0629272e114
Dmitriy <ddpemvr@mail.ru>
2025-03-23 21:50:48 +0500

    Main function and request processing

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+# Функция для возвращения статус кода подключения к сайту
+
 def statusCode(site_url):
     response = requests.get(site_url);
     return response
+
+def parser_to_site(site_url, output_format="json", output_file="tagil_news"):
+    # Проверка исключения при запросе к сайту
+    try:
+        statusCode(site_url).raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при запросе к сайту: {e}")
+        return
+    
+    # Сделаем так, чтобы BeautifulSoup прошел по странице и нашел отдельные блоки с новостями
+    
+    
+
 
